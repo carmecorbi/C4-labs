328bce08b369e736e635194bac9b9807448ab630
juditsalavedra <127085765+juditsalavedra@users.noreply.github.com>
2024-12-16 11:18:58 +0100

    update

--- Week2/utils.py
+++ Week2/utils.py
@@ -100,35 +100,44 @@ def DLT_homography(points1, points2):
     points2n = T2 @ points2_n
 
 
-    # Note: For each point correspondence, 2 linear equations are defined:
-    #           1. x(h_31·x + h_32·y + h_33) - (h_11·x + h_12·y + h_13) = 0  -->  (x - x' = 0)
-    #           2. y(h_31·x + h_32·y + h_33) - (h_21·x + h_22·y + h_23) = 0  -->  (y - y' = 0)
-    # Then can define the system of equations in a matricial form: Ah = 0
+    # Note: For each point correspondence ((x,y,z) in the 1st image and (u,v,w) in the 2nd image, 
+    #       where z and w are 1 after the normalization step), 2 linear equations are defined:
+    #
+    #           1. (h_11·x + h_12·y + h_13·1) - u(h_31·x + h_32·y + h_33·1) = 0  
+    #           2. (h_21·x + h_22·y + h_23·1) - v(h_31·x + h_32·y + h_33·1) = 0  
+    #
+    # Then we can define the system of equations in a matricial form: Ah = 0
     A = []
     n = points1.shape[1]
 
     for i in range(n):
+        # Get the normalized points
         x,y,z = points1n[0,i], points1n[1,i], points1n[2,i]
         u,v,w = points2n[0,i], points2n[1,i], points2n[2,i]
-        A.append([0,0,0,-w*x,-w*y,-w*z,v*x,v*y,v*z])
+        # From the 1st equation (w=1 and z=1)
         A.append([w*x, w*y, w*z, 0, 0, 0, -u*x, -u*y, -u*z])
+        # From the 2nd equation
+        A.append([0, 0, 0, -w*x, -w*y, -w*z, v*x, v*y, v*z])
+        
       
+    # Note: Solve Ah=0 using the SVD, so that we obtain A = U·D·V^T and the
+    #       solution h will be the last column of V (eigenvector associated to the 
+    #       smallest eigenvalue)
 
-    # Convert A to array
-
+    # Convert A to array and perform the SVD
     A = np.asarray(A)
-    U, d, Vt = np.linalg.svd(A)
+    U, D, Vt = np.linalg.svd(A)
 
-    # Extract homography (last line of Vt)
-    L = Vt[-1, :] / Vt[-1, -1]
-    H = L.reshape(3, 3)
+    # Extract homography (last row of Vt --> normalize it so that h_33 = 1)
+    h = Vt[-1, :] / Vt[-1, -1]
+    # Reshape to get homography matrix
+    H = h.reshape(3, 3) 
     
-    # Denormalise
+    # Denormalize to get the transfomration matrix on the original coord. system
     H = np.linalg.inv(T2) @ H @ T1
-    
-
 
     return H
+
 
 def Inliers(H, points1, points2, th):
     
@@ -137,19 +146,32 @@ def Inliers(H, points1, points2, th):
         idx = np.empty(1)
         return idx
     
-    
     # ToDo: complete this code .......
+    # Given:
+    #   - (x,y,z): point in the 1st image (points1)
+    #   - (u,v,w): point in the 2nd image (points2)
+    #   - H: homography matrix such that (x',y',z')^T = H·(x, y, z)^T and (u', v', w')^T = H^(-1)·(u, v, w)^T 
 
-    points1_transformed = Normalise_last_coord(H @points1)
-    points2_transfomed = Normalise_last_coord(np.linalg.inv(H)@points2)
+    # Compute transformed (normalized: 3rd coordinate equal to 1) points 
+    #   - Transform points1 to the 2nd image space
+    points1_transformed = Normalise_last_coord(H @ points1)
+    #   - Transform points2 to the 1st image space
+    points2_transfomed = Normalise_last_coord(np.linalg.inv(H) @ points2)
+    # Normalize given points
     points1 = Normalise_last_coord(points1)
     points2 = Normalise_last_coord(points2)
     
+    # Compute the reprojection error between all correspondences:
+    #   - Euclidean distance between tranformed points and their correspondences 
+    #     (combination of forward (points2-points1_transf) and backward 
+    #     (points1-points2_transf) reprojection error)
     error = np.sqrt((np.sum((points2[:-1] - points1_transformed[:-1])**2, axis=0) + np.sum((points2_transfomed[:-1] - points1[:-1])**2, axis=0)))
+    # Get as inliers the ones for which the reprojection error is under a threshold
     inliers_mask = error < th
     inliers_indices = np.where(inliers_mask)[0]
     
     return inliers_indices
+
 
 def Ransac_DLT_homography(points1, points2, th, max_it):
     
@@ -161,10 +183,12 @@ def Ransac_DLT_homography(points1, points2, th, max_it):
     while it < max_it:
         # Take 4 point correspondences between the 2 images (randomly chosen)
         indices = random.sample(range(1, Npts), 4)
+        # Estimate the homography matrix H using the DLT algorithm
         H = DLT_homography(points1[:,indices], points2[:,indices])
+        # Get the indices of the points that are considered inliers
         inliers = Inliers(H, points1, points2, th)
         
-        # test if it is the best model so far
+        # test if it is the best model so far based on the number of inliers
         if inliers.shape[0] > best_inliers.shape[0]:
             best_inliers = inliers
         
